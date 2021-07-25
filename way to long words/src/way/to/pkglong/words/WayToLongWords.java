5744197bfe7d5fdf3b13d514d06d3eb134584144
hariswp-16 <wahyupretetet004@gmail.com>
2021-07-25 23:30:54 +0800

    Upload Tugas Harian

---
+++ way to long words/src/way/to/pkglong/words/WayToLongWords.java
@@ -0,0 +1,23 @@
+package way.to.pkglong.words;
+
+import java.util.Scanner;
+
+
+
+public class WayToLongWords {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        while (n-- > 0) {
+            String str = sc.next();
+            int len = str.length();
+            if (len > 10) {
+                System.out.println("" + str.charAt(0) + (len - 2) + str.charAt(len - 1));
+
+            } else {
+                System.out.println(str);
+            }
+        }
+    }
+}
